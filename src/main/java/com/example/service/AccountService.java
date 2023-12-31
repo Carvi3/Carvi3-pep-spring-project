5457bfbbd3297f889958ac71e96301ae7f6bae93
Andrew Carvajal <andrewcarvajal38@gmail.com>
2023-12-31 05:31:42 +0000

    v.1.01-12.31 Switching computers

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,32 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+    
+    @Autowired
+    private AccountRepository accountRepo;
+
+    public boolean checkUsername(String username){
+        return username.length() != 0;
+    }
+    public boolean checkPassword(String password){
+        return password.length() >= 4;
+    }
+
+    public Account register(Account account){
+        if(checkUsername(account.getUsername()) && checkPassword(account.getPassword())){
+            return accountRepo.save(account);
+        }
+        return null;
+    }
+
+    //public Account Login(Account acount){
+
+    //}
 }
